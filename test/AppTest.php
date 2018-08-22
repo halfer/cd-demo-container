1bc46e5fff4a1d0d8718aa60bf3779486c21873b
Jon Hinks <projects.ws@jondh.me.uk>
2018-08-22 19:08:00 +0100

    Add a skeleton test class

---
+++ test/AppTest.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * A PHP test to check the app is OK
+ */
+
+use PHPUnit\Framework\TestCase;
+
+class AppTest extends TestCase
+{
+    public function testApp()
+    {
+        $this->markTestIncomplete();
+    }
+}
