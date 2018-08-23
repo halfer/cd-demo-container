c853704b7b5003e0443b724b09a6efcb030af3b5
Jon Hinks <projects.ws@jondh.me.uk>
2018-08-23 12:45:57 +0100

    Try to reach into the container to get a web page

--- test/AppTest.php
+++ test/AppTest.php
@@ -11,5 +11,26 @@ class AppTest extends TestCase
     public function testApp()
     {
         $this->markTestIncomplete();
+        print_r($_SERVER);
+        echo $this->getWebPage() . "\n";
+    }
+
+    protected function getWebPage()
+    {
+        $output = $return = null;
+        exec('docker exec -ti cd-demo-container wget -qO- http://localhost', $output, $return);
+
+        // Check return value first
+        if ($return)
+        {
+            throw new RuntimeException(
+                sprintf(
+                    'Returned a failure exit code %d on Docker operation',
+                    $return
+                )
+            );
+        }
+
+        return $output;
     }
 }
