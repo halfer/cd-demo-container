38286c84cf84c5fce1552d52c94ccbfeb5633a6c
Jon Hinks <projects.ws@jondh.me.uk>
2018-08-31 16:05:09 +0100

    Add a version number in an env var taken from the Git tag

--- web/index.php
+++ web/index.php
@@ -7,6 +7,9 @@
             This is a 'hello world' for my CD container.
         </div>
         <p>
+            Version: <?php echo getenv('CD_DEMO_VERSION') ?>
+        </p>
+        <p>
             GUID: <?php echo file_get_contents(__DIR__ . '/guid.txt') ?>
         </p>
     </body>
